339a94727d3ee9e2decbc6001a6dc0a16564127a
Nabeel Ahmed <nabeel.amd93@gmail.com>
2026-02-04 21:15:50 -0600

    ETL-AUTH:#1

--- src/main/java/com/barco/common/CommonApplication.java
+++ src/main/java/com/barco/common/CommonApplication.java
@@ -1,7 +1,11 @@
 package com.barco.common;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author Nabeel Ahmed
@@ -9,8 +13,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CommonApplication {
 
+	public Logger logger = LogManager.getLogger(CommonApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(CommonApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return (args) -> {
+			logger.info("Common Application Started Successfully");
+		};
 	}
 
 }
